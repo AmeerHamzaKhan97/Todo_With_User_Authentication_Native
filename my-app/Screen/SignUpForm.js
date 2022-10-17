40efe930e67189beb77ecf5f558f7b74b4a37099
AmeerHamzaKhan97 <hamzakn97@gmail.com>
2022-10-17 19:01:11 +0530

    updating the signupform

--- my-app/Screen/SignUpForm.js
+++ my-app/Screen/SignUpForm.js
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  Platform,
+  TextInput,
+} from "react-native";
 import Input from "../components/Input";
 import Button from "../UI/Button";
 import FlatButton from "../UI/FlatButton";
 import { Domain } from "../Domain";
 import UserContext from "../context/UserContext";
+import * as Animatable from "react-native-animatable";
+import LinearGradient from "react-native-linear-gradient";
+import { Ionicons } from "@expo/vector-icons";
 
 function SignUpForm({ navigation }) {
   const [email, setEmail] = useState("");
@@ -42,23 +53,52 @@ function SignUpForm({ navigation }) {
       });
   };
   return (
-    <View style={styles.authContent}>
-      <View>
-        <Input
-          label="Email Address"
-          onUpdateValue={updateEmailHandler}
-          //  onUpdateValue={updateInputValueHandler.bind(this, 'email')}
-          //  value={enteredEmail}
-          keyboardType="email-address"
+    // <View style={styles.authContent}>
+    //   <View>
+    //     <Input
+    //       label="Email Address"
+    //       onUpdateValue={updateEmailHandler}
 
-          //  isInvalid={emailIsInvalid}
-        />
-        <Input label="Password" onUpdateValue={updatePasswordHandler} />
-        <View style={styles.buttons}>
-          <Button onPress={saveUser}>Signup</Button>
+    //       keyboardType="email-address"
+
+    //     />
+    //     <Input label="Password" onUpdateValue={updatePasswordHandler} />
+    //     <View style={styles.buttons}>
+    //       <Button onPress={saveUser}>Signup</Button>
+    //     </View>
+    //     <Text>Hello,{user.email}</Text>
+    //     <Text>{user.auth ? "TRUE" : "FALSE"}</Text>
+    //   </View>
+    // </View>
+    <View style={styletubes.container}>
+      <View style={styletubes.header}>
+        <Text style={styletubes.text_header}>Welcome!</Text>
+      </View>
+      <View style={styletubes.footer}>
+        <Text style={styletubes.text_footer}>Email</Text>
+        <View style={styletubes.action}>
+          <Ionicons name="person-outline" size={20} />
+          <TextInput
+            placeholder="Your Email"
+            style={styletubes.textInput}
+            autoCapitalize="none"
+            keyboardType="email-address"
+          />
+          <Ionicons name="checkmark-done-circle-outline" size={20} />
         </View>
-        <Text>Hello,{user.email}</Text>
-        <Text>{user.auth ? "TRUE" : "FALSE"}</Text>
+        <Text style={[styletubes.text_footer, { marginTop: 35 }]}>
+          Password
+        </Text>
+        <View style={styletubes.action}>
+          <Ionicons name="lock-closed-outline" size={20} />
+          <TextInput
+            placeholder="Your Password"
+            style={styletubes.textInput}
+            autoCapitalize="none"
+            // keyboardType="pa"
+          />
+          <Ionicons name="eye-off-outline" size={20} />
+        </View>
       </View>
     </View>
   );
@@ -81,5 +121,74 @@ const styles = StyleSheet.create({
   },
   buttons: {
     marginTop: 12,
+  },
+});
+
+const styletubes = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#009387",
+  },
+  header: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 20,
+    paddingBottom: 50,
+  },
+  footer: {
+    flex: 3,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  text_header: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  text_footer: {
+    color: "#05375a",
+    fontSize: 18,
+  },
+  action: {
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#f2f2f2",
+    paddingBottom: 5,
+  },
+  actionError: {
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#FF0000",
+    paddingBottom: 5,
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS === "ios" ? 0 : -12,
+    paddingLeft: 10,
+    color: "#05375a",
+  },
+  errorMsg: {
+    color: "#FF0000",
+    fontSize: 14,
+  },
+  button: {
+    alignItems: "center",
+    marginTop: 50,
+  },
+  signIn: {
+    width: "100%",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  textSign: {
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
